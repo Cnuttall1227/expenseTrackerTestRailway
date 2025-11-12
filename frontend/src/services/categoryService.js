5fc7322e114f1eb6ef6130f5925e10ee0599f864
Cnuttall1227 <53721838+Cnuttall1227@users.noreply.github.com>
2025-11-11 22:38:24 -0600

    Update categoryService.js updated API calls

--- frontend/src/services/categoryService.js
+++ frontend/src/services/categoryService.js
@@ -1,16 +1,16 @@
 const API_URL =
-  process.env.REACT_APP_API_URL_CATEGORIES || "'https://my-backend.up.railway.app/api/categories'";
+  process.env.REACT_APP_API_URL || "https://expensetrackertestrailway-production.up.railway.app";
 
 //Fetch categories
 export const getCategories = async () => {
-  const response = await fetch(API_URL);
+  const response = await fetch(`${API_URL}/api/categories`);
   if (!response.ok) throw new Error("Failed to fetch categories");
   return await response.json();
 };
 
 //Add categories
 export const addCategory = async (category) => {
-  const response = await fetch(API_URL, {
+  const response = await fetch(`${API_URL}/api/categories`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(category),
