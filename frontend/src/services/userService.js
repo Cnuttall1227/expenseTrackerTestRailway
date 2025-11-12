25be79e4de1aacba13cd91b4867ffac5058e8d22
Cnuttall1227 <53721838+Cnuttall1227@users.noreply.github.com>
2025-11-11 22:39:24 -0600

    Update userService.js updated API calls

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,16 +1,16 @@
 const API_URL =
-  process.env.REACT_APP_API_URL_USERS || 'https://my-backend.up.railway.app/api/users';
+  process.env.REACT_APP_API_URL || "https://expensetrackertestrailway-production.up.railway.app";
 
 //Fetch users
 export const getUsers = async () => {
-  const response = await fetch(API_URL);
+  const response = await fetch(`${API_URL}/api/users`);
   if (!response.ok) throw new Error("Failed to fetch users");
   return await response.json();
 };
 
 //Add new users
 export const addUser = async (user) => {
-  const response = await fetch(API_URL, {
+  const response = await fetch(`${API_URL}/api/users`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
